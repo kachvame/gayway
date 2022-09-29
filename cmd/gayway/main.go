dead29ba4c49010cf7125c9f12c787f8d29a4082
invakid404 <invakid404@riseup.net>
2022-09-30 00:26:25 +0300

    feat: set up zerolog

--- cmd/gayway/main.go
+++ cmd/gayway/main.go
@@ -2,13 +2,21 @@ package main
 
 import (
 	"github.com/bwmarrin/discordgo"
-	"log"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"github.com/rs/zerolog/pkgerrors"
 	"os"
 	"os/signal"
 	"syscall"
 )
 
 func run() error {
+	if err := setupLogger(); err != nil {
+		return err
+	}
+
+	discordLogger := log.With().Str("component", "bot").Logger()
+
 	discord, err := discordgo.New("Bot " + os.Getenv("GAYWAY_DISCORD_TOKEN"))
 	if err != nil {
 		return err
@@ -18,7 +26,7 @@ func run() error {
 		return err
 	}
 
-	log.Println("opened discord client")
+	discordLogger.Info().Msg("opened discord client")
 
 	defer func(discord *discordgo.Session) {
 		_ = discord.Close()
@@ -31,8 +39,36 @@ func run() error {
 	return nil
 }
 
+func setupLogger() error {
+	logLevelEnv := os.Getenv("GAYWAY_LOG_LEVEL")
+	if logLevelEnv == "" {
+		logLevelEnv = "info"
+	}
+
+	logLevel, err := zerolog.ParseLevel(logLevelEnv)
+	if err != nil {
+		return err
+	}
+	log.Logger = log.Level(logLevel)
+
+	if os.Getenv("GAYWAY_DEV") == "true" {
+		log.Logger = log.
+			Output(zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: zerolog.TimeFormatUnix}).
+			With().
+			Caller().
+			Logger()
+	}
+
+	return nil
+}
+
 func main() {
+	zerolog.ErrorStackMarshaler = pkgerrors.MarshalStack
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+	zerolog.DefaultContextLogger = &log.Logger
+
 	if err := run(); err != nil {
-		log.Fatalln("gayway: ", err)
+		log.Error().Err(err)
+		os.Exit(1)
 	}
 }
