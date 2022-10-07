dead146280279211909a379f83cd8050874a5c83
invakid404 <invakid404@riseup.net>
2022-10-08 00:02:25 +0300

    feat(grpcgen/generator): parse package & find entrypoint struct

--- grpcgen/generator.go
+++ grpcgen/generator.go
@@ -2,7 +2,12 @@ package grpcgen
 
 import (
 	_ "embed"
+	"fmt"
+	"github.com/kachvame/gayway/codegen"
 	"github.com/rs/zerolog"
+	"go/token"
+	"go/types"
+	"golang.org/x/tools/go/packages"
 )
 
 type Generator struct {
@@ -25,5 +30,31 @@ func NewGenerator(logger zerolog.Logger, packageName, entrypointStruct string) *
 }
 
 func (generator *Generator) Run() error {
+	packageConfig := &packages.Config{
+		Mode: packages.NeedTypes | packages.NeedSyntax,
+		Fset: token.NewFileSet(),
+	}
+
+	discordgoPackages, err := packages.Load(packageConfig, generator.packageName)
+	if err != nil {
+		return fmt.Errorf("failed to load discordgo package: %w", err)
+	}
+
+	if packages.PrintErrors(discordgoPackages) > 0 {
+		return fmt.Errorf("errors while loading discordgo package")
+	}
+
+	generator.logger.Info().Msg("parsed discordgo package")
+
+	discordgoPackage := discordgoPackages[0]
+	typesScope := discordgoPackage.Types.Scope()
+
+	_, sessionStruct := codegen.LookupType[*types.Named](typesScope, generator.entrypointStruct)
+	if sessionStruct == nil {
+		return fmt.Errorf("failed to find entrypoint struct '%s'", generator.entrypointStruct)
+	}
+
+	generator.logger.Info().Msgf("found entrypoint struct '%s'", generator.entrypointStruct)
+
 	return nil
 }
